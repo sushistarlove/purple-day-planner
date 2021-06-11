e1f17d06ac184fc45e356fba3af0fe9e225a6d65
Gunnar Ulatowski <gunnarulatowski@outlook.com>
2021-06-11 18:54:32 -0400

    Added current day and timeblocks

---
+++ script.js
@@ -0,0 +1,6 @@
+var today = moment();
+$("#currentDay").text(today.format("dddd, MMMM Do YYYY"));
+
+//When I click save, I capture the value of the input field
+//When I click save, (See dom traversal)
+
